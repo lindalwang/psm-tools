9a85b4959f980d197d5a68148d3612242c04c926
lindalwang <85533450+lindalwang@users.noreply.github.com>
2021-07-19 15:05:29 -0700

    Update find_workload.py
    2nd commit, addressed comments and made changes

--- pyclient/apps/find_workload.py
+++ pyclient/apps/find_workload.py
@@ -1,18 +1,20 @@
+#!/usr/bin/python3
 
 import os
 from typing import ItemsView
 from apigroups.client.apis import WorkloadV1Api
 from apigroups.client import configuration, api_client
-import pytz 
-import time
-from tzlocal import get_localzone
+from tabulate import tabulate
 import argparse
+from tzlocal import get_localzone
 import datetime
 import warnings
+import re
 
 warnings.simplefilter("ignore")
 
 HOME = os.environ['HOME']
+
 
 configuration = configuration.Configuration(
     psm_config_path=HOME+"/.psm/config.json",
@@ -25,41 +27,61 @@ api_instance = WorkloadV1Api(client)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--age", dest="age", help="creation date: <number>d|w|m")
-parser.add_argument("--dsc", dest="dsc", help = "MAC address of DSC" )
-parser.add_argument("--label", dest="label", help = 'label of workload')
+parser.add_argument(dest = "Quick Command", action='store_true', 
+help = "'find_workload.py --age 1w' prints workload from recent one week")
+parser.add_argument("--age", dest="age", help="creation date: <number>d|w|m")
+parser.add_argument("--dsc", dest="dsc", help = "name of DSC" )
+parser.add_argument("--label", dest="label", help = 'label of workload, e.g. --label key:value')
 parser.add_argument("--host", dest="host", help = 'host name of workload')
+parser.add_argument("--tenant", dest="tenant", help = 'tenant name, if not specified, default')
+parser.add_argument("--json", dest="json", action="store_true", help = 'output in json format')
+
 args = parser.parse_args()
 
 
-workload = api_instance.list_workload(o_tenant = 'default')
-items = workload.get('items')
+if args.tenant:
+    tenant = args.tenant
+else:
+    tenant = 'default'
+
+workload = api_instance.list_workload(o_tenant = tenant)
+items = workload['items']
 workload_list = []
 new_item_list =[]
 
 
-#adjusting timezone difference: UTC -> PST
-PST = pytz.timezone('US/Pacific')
-utc_dt = datetime.datetime.now()
-current_time = utc_dt.astimezone(PST)
+current_time = datetime.datetime.now(get_localzone())
 desired_time = datetime.datetime.now()
 
+
 if args.age:
-    date_number = int(args.age[0:-1])
-    date_type = args.age[-1]
-    if date_type == "m":
-        desired_time = current_time - datetime.timedelta(days = ( int(date_number) * 31 ))
-    elif date_type == "w":
-        desired_time = current_time - datetime.timedelta(weeks = int(date_number))
-    elif date_type == "d":
-        desired_time = current_time - datetime.timedelta(days = int(date_number))
+    #if user does not specify time length, the default unit is weeks
+    if args.age.isnumeric():
+        print("Since no time unit is specified, recent workload from " + args.age + " weeks are returned.")
+        desired_time = current_time - datetime.timedelta(weeks = int(args.age))
+    elif args.age.isalpha():
+        print("Please enter valid input. e.g. --age 3d")
+        exit()
+    else:
+        date_type = "".join(re.split("[^a-zA-Z]*", args.age))
+        date_number = "".join(re.split("[^0-9]*", args.age))
+        if date_type == "m" or "month" in date_type:
+            desired_time = current_time - datetime.timedelta(days = ( int(date_number) * 31 ))
+        elif date_type == "w" or "week" in date_type:
+            desired_time = current_time - datetime.timedelta(weeks = int(date_number))
+        elif date_type == "d" or "day" in date_type:
+            desired_time = current_time - datetime.timedelta(days = int(date_number))
+        else:
+            print("Please enter valid input. e.g. --age 5w")
+            exit()
     for x in items:
         if desired_time < x['meta']['creation_time']: 
             workload_list.append(x)   
 
-#set default creation_time: recent 2 months workload
-if not args.age:
-    desired_time = current_time - datetime.timedelta(weeks = int(8))
+#set default_time: recent 8 weeks workload when user does not input age
+else:
+    default_time = 8
+    desired_time = current_time - datetime.timedelta(weeks = int(default_time))
     for item in items:
             if desired_time < item['meta']['creation_time']: 
                 workload_list.append(item)
@@ -69,7 +91,7 @@ if args.dsc:
     new_item_list = workload_list
     workload_list = []
     for item in new_item_list:
-        if args.dsc in item.get('spec').get('interfaces')[0].get('mac_address'):
+        if args.dsc in item['spec']['interfaces'][0]['mac_address']:
             workload_list.append(item)
 
 
@@ -77,22 +99,51 @@ if args.host:
     new_item_list = workload_list
     workload_list = []
     for item in new_item_list:
-        if args.host in item.get('spec').get('host_name'):
+        if args.host in item['spec']['host_name']:
             workload_list.append(item)
 
 
 if args.label:
-    key = args.label.split(":")[0]
-    value = args.label.split(":")[1]
-    new_item_list = workload_list
-    workload_list = []
-    for item in new_item_list:
-        if item.get('meta').get('labels'):
-            if value in item.get('meta').get('labels').get(key):
-                workload_list.append(item)
+    if args.label.find(':') == -1:
+        print('Please input label in correct format e.g. --label key:value')
+        exit()
+    else:
+        key = args.label.split(":")[0]
+        value = args.label.split(":")[1]
+        new_item_list = workload_list
+        workload_list = []
+        for item in new_item_list:
+            label = item['meta']['labels']
+            if label and key in label.keys():
+                if value in label.get(key):
+                    workload_list.append(item)
 
 
-if not workload_list:
-    print('There are no workloads matching the descriptions.')
 if workload_list:
-    print(workload_list)
+    if args.json:
+        print(workload_list)
+    else:
+        final_dict = []
+        labels = []
+        for item in workload_list:
+            workloads = []
+            workloads.append(item['meta']['name'])
+            workloads.append(item['meta']['creation_time'])
+            if item.get('meta').get('labels'):
+                string_label = ""
+                for key, value in item.get('meta').get('labels').items():
+                    string_label += key + ':' + value + "   "
+                workloads.append(string_label)
+            else:
+                workloads.append("n/a")
+            workloads.append(item['meta']['tenant'])
+            workloads.append(item['spec']['host_name'])
+            workloads.append(item['spec']['interfaces'][0]['mac_address'])
+            workloads.append(item['status']['interfaces'][0]['external_vlan'])
+            workloads.append(item['status']['interfaces'][0]['network'])
+            final_dict.append(workloads)
+        print(tabulate(final_dict, headers=["workload-name", "creation_time", "labels", "tenant", "host_name", "mac_address", "external_vlan", "network"]))
+        
+else:
+    print('There are no workloads matching the descriptions.')
+        
